7086e6f72ec7d23ad7f378600d80eeb167c0c466
RasmusKodehode <rasmus.kodehode@gmail.com>
2026-06-02 14:48:05 +0200

    added server action

---
+++ app/actions.ts
@@ -0,0 +1,8 @@
+"use server";
+
+export async function getMockData() {
+  return [
+    { id: 1, name: "Item 1" },
+    { id: 2, name: "Item 2" },
+  ];
+}
--- app/bestill-reise/page.tsx
+++ app/bestill-reise/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import { getMockData } from "../actions";
 
-export default function BookTravel() {
+export default async function BookTravel() {
+  const data = await getMockData();
+  console.log(data);
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
       <main className="flex flex-1 w-full flex-col items-center justify-between px-18 py-32 bg-white sm:items-start">

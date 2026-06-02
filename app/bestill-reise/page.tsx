e5a9df4b7fd3528af1154b6f1c96f1a3b238d1af
RasmusKodehode <rasmus.kodehode@gmail.com>
2026-06-02 15:43:12 +0200

    created mock data

--- app/bestill-reise/page.tsx
+++ app/bestill-reise/page.tsx
@@ -2,8 +2,7 @@ import Image from "next/image";
 import { getMockData } from "../actions";
 
 export default async function BookTravel() {
-  const data = await getMockData();
-  console.log(data);
+  const data: any = await getMockData();
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
       <main className="flex flex-1 w-full flex-col items-center justify-between px-18 py-32 bg-white sm:items-start">

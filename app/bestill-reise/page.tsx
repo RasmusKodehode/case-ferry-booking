4cf4440022b769744beb0754ba7da19304f847fb
RasmusKodehode <rasmus.kodehode@gmail.com>
2026-06-02 13:52:28 +0200

    removed center column

--- app/bestill-reise/page.tsx
+++ app/bestill-reise/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function BookTravel() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 bg-white sm:items-start">
+      <main className="flex flex-1 w-full flex-col items-center justify-between px-18 py-32 bg-white sm:items-start">
         Bestill reise her!
       </main>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 bg-white sm:items-start">
-        <Link href="/bestill-reise" className="w-fit px-3 py-2 bg-red-600">Book din tur med Fjordline her!</Link>
+      <main className="flex flex-1 w-full flex-col items-center justify-between px-18 py-32 bg-white sm:items-start">
+        <Link href="/bestill-reise" className="w-fit px-3 py-2 bg-red-600 text-white">Book din tur med Fjordline her!</Link>
       </main>
     </div>
   );

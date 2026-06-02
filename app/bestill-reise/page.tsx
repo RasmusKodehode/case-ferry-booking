814ef6bb3a8ba26bd58848ad351212915a3b30ab
RasmusKodehode <rasmus.kodehode@gmail.com>
2026-06-02 13:28:26 +0200

    create page for booking

---
+++ app/bestill-reise/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function BookTravel() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 bg-white sm:items-start">
+        Bestill reise her!
+      </main>
+    </div>
+  );
+}

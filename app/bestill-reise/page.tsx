ce92a280b3083eaa98461ea9855b3b3a2c91311c
RasmusKodehode <rasmus.kodehode@gmail.com>
2026-06-07 12:59:02 +0200

    fixed issue where confirm button didn't work

--- app/bestill-reise/page.tsx
+++ app/bestill-reise/page.tsx
@@ -88,12 +88,16 @@ export default function BookTravel() {
   };
 
   const handleConfirm = () => {
-    if (!selectedDeparture || !selectedDate) return;
+    if (selectedDeparture === null || selectedDate === null) {
+      console.log("no departure or no date");
+      return;
+    };
     setBookingData({
       date: selectedDate,
       departure: selectedDeparture,
     });
     router.push("/bekreft-reise");
+    console.log("confirm booking");
   };
 
   useEffect(() => {
@@ -181,6 +185,7 @@ export default function BookTravel() {
           <>
             <p>{`You have selected ${getDate(filteredDepartures[selectedDeparture].ETD)} at ${getTime(filteredDepartures[selectedDeparture].ETD)} from ${filteredDepartures[selectedDeparture].departure} to ${filteredDepartures[selectedDeparture].arrival}`}</p>
             <button
+              type="button"
               onClick={handleConfirm}
               className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
             >

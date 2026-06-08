a64d23111347a26d84ff9e496d706cc37072c787
RasmusKodehode <rasmus.kodehode@gmail.com>
2026-06-08 11:54:48 +0200

    introduced Hamburger

--- components/ui/navBar.tsx
+++ components/ui/navBar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import Block from "../../public/Block.png";
+import HamburgerMenu from "./Hamburger";
 
 export default function NavBar() {
   return (
@@ -10,7 +11,10 @@ export default function NavBar() {
       <Link href="/">
       <Image src={Block} width={100} height={100} alt="Logo med link til hjemmeside" className="w-auto h-10" />
       </Link>
-      <div className="flex flex-row gap-3 h-fit items-center">
+      <div className="flex md:hidden">
+        <HamburgerMenu />
+      </div>
+      <div className="md:flex md:flex-row md:gap-3 md:h-fit md:items-center hidden">
         <Link href="/bestill-reise">Bestill reise</Link>
         <div>Våre ruter</div>
         <div>Kontakt oss</div>

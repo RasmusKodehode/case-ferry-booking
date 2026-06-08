723d4686511781f9a10e11943d1e7e04157ccc75
RasmusKodehode <rasmus.kodehode@gmail.com>
2026-06-08 13:53:57 +0200

    mobile design landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,38 @@
 import Image from "next/image";
 import Link from "next/link";
+import Banner from "../public/Banner.png";
+import { FaSearch } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa6";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
-      <main className="flex flex-1 w-full flex-col items-center justify-between px-18 py-32 bg-white sm:items-start">
-        <Link href="/bestill-reise" className="w-fit px-3 py-2 bg-red-600 text-white">Book din tur med Fjordline her!</Link>
-      </main>
-    </div>
+    <main className="flex flex-1 w-full flex-col gap-4 bg-white">
+      <div className="flex w-full m-0 p-0">
+        <Image
+          src={Banner}
+          width={1000}
+          height={1000}
+          alt="Bilde af fjordline båt"
+        />
+      </div>
+      <div className="flex flex-col justify-baseline gap-1.5 px-6">
+        <p className="text-lg">
+          Reis i sommer med <span className="text-red-500">komfort</span>
+        </p>
+        <p className="text-sm">
+          Hvor i <span className="text-red-500">Danmark</span> skal du i sommer?
+        </p>
+        <Link
+          href="/bestill-reise"
+          className="flex flex-row justify-between w-full mt-4 px-3 py-2 bg-red-600 text-white items-center rounded-lg"
+        >
+          <div className="flex flex-row items-center gap-2">
+            <FaSearch />
+            <p>Finn min reise</p>
+          </div>
+          <FaArrowRight />
+        </Link>
+      </div>
+    </main>
   );
 }

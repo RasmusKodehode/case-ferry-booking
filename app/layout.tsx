0a5ecf02370a1ca04f1110987c12a95eec680954
RasmusKodehode <rasmus.kodehode@gmail.com>
2026-06-08 10:57:05 +0200

    install tailwind and create config file

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { BookingProvider } from "@/context/BookingContext";
+import NavBar from "@/components/ui/navBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <NavBar />
         <BookingProvider>{children}</BookingProvider>
       </body>
     </html>
--- components/ui/navBar.tsx
+++ components/ui/navBar.tsx
@@ -2,7 +2,9 @@
 
 export default function NavBar() {
   return (
-    <div>
+    <div className="flex flex-row justify-between w-full h-14 px-3 py-7 bg-primary">
+        <div>Home</div>
+        <div>Bestill reise</div>
     </div>
   );
 }

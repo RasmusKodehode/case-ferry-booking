aaf8095e6ec895fc3502ab01c97c404eb8543abe
RasmusKodehode <rasmus.kodehode@gmail.com>
2026-06-08 11:38:47 +0200

    finished Navbar

--- components/ui/navBar.tsx
+++ components/ui/navBar.tsx
@@ -1,10 +1,22 @@
 "use client";
 
+import Link from "next/link";
+import Image from "next/image";
+import Block from "../../public/Block.png";
+
 export default function NavBar() {
   return (
-    <div className="flex flex-row justify-between w-full h-14 px-3 py-7 bg-primary">
-        <div>Home</div>
-        <div>Bestill reise</div>
+    <div className="flex flex-row justify-between w-full h-20 px-3 py-7 bg-primary">
+      <Link href="/">
+      <Image src={Block} width={100} height={100} alt="Logo med link til hjemmeside" className="w-auto h-10" />
+      </Link>
+      <div className="flex flex-row gap-3 h-fit items-center">
+        <Link href="/bestill-reise">Bestill reise</Link>
+        <div>Våre ruter</div>
+        <div>Kontakt oss</div>
+        <div className="bg-red-500 p-1.5 rounded-lg text-white">Min Side</div>
+        <div className="bg-black p-1.5 rounded-lg text-white">Registrer</div>
+      </div>
     </div>
   );
 }
